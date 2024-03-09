633eea7276f63a726dc3135f229b7802175d5a6c
abdul ahad <abdul_ahad1996@live.com>
2024-03-09 01:27:25 +0100

    - add adapter pattern

---
+++ AdapterInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdapterInPracticeApp.swift
+//  AdapterInPractice
+//
+//  Created by macbook abdul on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdapterInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AdapterInPractice
+//
+//  Created by macbook abdul on 03/03/2024.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+//class MapsViewController:UIVIE
